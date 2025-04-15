43bdba6714e018eb9379e1c825654aa4dedafa28
dsstaub <DavidStaub@gmail.com>
2025-04-14 23:39:49 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', function(event) {
+  // Perform install steps
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
