204e9b2e16d729ba98f9f6af728666adb58f7e00
dsstaub <196190529+dsstaub@users.noreply.github.com>
2025-04-15 20:26:33 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// app.js +
+// app.js
 
 window.addEventListener('DOMContentLoaded', () => {
   document.body.classList.remove('opacity-0');
@@ -15,4 +15,4 @@ function toggleSection(id) {
   const isOpen = section.style.maxHeight && section.style.maxHeight !== '0px';
   section.style.maxHeight = isOpen ? '0' : section.scrollHeight + 'px';
   arrow.style.transform = isOpen ? 'rotate(0deg)' : 'rotate(180deg)';
- }
+}
