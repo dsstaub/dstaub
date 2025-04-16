1f39192b3bab5f766cc6a232c29bac0498c6ce6b
dsstaub <196190529+dsstaub@users.noreply.github.com>
2025-04-15 22:02:31 -0400

    cache version update

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 
-const CACHE_VERSION = 'v24'; // Increment this value with each deployment
+const CACHE_VERSION = 'v25'; // Increment this value with each deployment
 const CACHE_NAME = `my-pwa-cache-${CACHE_VERSION}`;
 
 const urlsToCache = [
