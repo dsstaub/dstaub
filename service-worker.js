7d061e86257235e57a7faf7c8d925c765bfb0908
dsstaub <196190529+dsstaub@users.noreply.github.com>
2025-04-16 21:58:17 -0400

    Update index.html with new content structure

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 
-const CACHE_VERSION = 'v49'; // Increment this value with each deployment
+const CACHE_VERSION = 'v50'; // Increment this value with each deployment
 const CACHE_NAME = `my-pwa-cache-${CACHE_VERSION}`;
 
 const urlsToCache = [
