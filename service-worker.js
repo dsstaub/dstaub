e3b8f068c0a801c7bb0d40ba6604aedfab48c9c3
dsstaub <196190529+dsstaub@users.noreply.github.com>
2025-04-21 18:05:55 -0400

    Add modal functionality and update index.html content

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 
-const CACHE_VERSION = 'v82'; // Increment this value with each deployment
+const CACHE_VERSION = 'v83'; // Increment this value with each deployment
 const CACHE_NAME = `my-pwa-cache-${CACHE_VERSION}`;
 
 const urlsToCache = [
