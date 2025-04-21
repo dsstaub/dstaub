ac15f1b808213da13e5e8bbf702d3e398a28be97
dsstaub <196190529+dsstaub@users.noreply.github.com>
2025-04-21 13:48:51 -0400

    Update logo size in layout.js header section

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 
-const CACHE_VERSION = 'v63'; // Increment this value with each deployment
+const CACHE_VERSION = 'v64'; // Increment this value with each deployment
 const CACHE_NAME = `my-pwa-cache-${CACHE_VERSION}`;
 
 const urlsToCache = [
